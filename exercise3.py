76db95760b41ba2bf58e011d1a55ab6f11cb7b45
Yashvi <yashvi.c.shah@gmail.com>
2015-09-27 22:44:18 -0400

    v1 exercise 3

--- exercise3.py
+++ exercise3.py
@@ -25,8 +25,33 @@ def diagnose_car():
     Errors:
 
     """
+    silent_car = raw_input('Is the car silent when you turn the key?')
+    if silent_car == "yes":
+        terminals_corroded = raw_input('Are the battery terminals corroded?')
+        if terminals_corroded == "yes":
+            print "Please clean terminals and try starting again."
+        elif terminals_corroded == "no":
+            print("The battery cables may be damaged. Replace cables and try again.")
+    elif silent_car == "no":
+        clicking_noise = raw_input('Does the car make a clicking noise?')
+        if clicking_noise == "yes":
+                print("Replace the battery.")
+        elif clicking_noise == "no":
+            crank_up = raw_input('Does the car crank up but fail to start?')
+            if crank_up == "yes":
+                print("Check spark plug connections.")
+            elif crank_up == "no":
+                engine_die = raw_input('Does the engine start and then die?')
+                if engine_die == "yes":
+                    fuel_injection = raw_input("Does your car have fuel injection?")
+                    if fuel_injection == "yes":
+                        print("Check to ensure the choke is opening and closing.")
+                    elif fuel_injection == "no":
+                        print("Get it in for service.")
+    else:
+        print ("Please enter either yes or no.")
 
-    print("The battery cables may be damaged. Replace cables and try again.")
+
 
 
 diagnose_car()
