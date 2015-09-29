7c0d42355f49437d546fe6af67231d0f87e8a5e5
Yashvi <yashvi.c.shah@gmail.com>
2015-09-29 15:30:49 -0400

    v2 exercise 3

--- exercise3.py
+++ exercise3.py
@@ -18,40 +18,48 @@ def diagnose_car():
     Interactively queries the user with yes/no questions to identify a
     possible issue with a car.
 
-    Inputs:
+    Inputs: yes and no questions
 
-    Expected Outputs:
+    Expected Outputs: more questions or solutions
 
-    Errors:
+    Errors: if something else is inserted other than yes or no, send error message
 
     """
-    silent_car = raw_input('Is the car silent when you turn the key?')
-    if silent_car == "yes":
-        terminals_corroded = raw_input('Are the battery terminals corroded?')
-        if terminals_corroded == "yes":
-            print "Please clean terminals and try starting again."
-        elif terminals_corroded == "no":
-            print("The battery cables may be damaged. Replace cables and try again.")
-    elif silent_car == "no":
-        clicking_noise = raw_input('Does the car make a clicking noise?')
-        if clicking_noise == "yes":
-                print("Replace the battery.")
-        elif clicking_noise == "no":
-            crank_up = raw_input('Does the car crank up but fail to start?')
-            if crank_up == "yes":
-                print("Check spark plug connections.")
-            elif crank_up == "no":
-                engine_die = raw_input('Does the engine start and then die?')
-                if engine_die == "yes":
-                    fuel_injection = raw_input("Does your car have fuel injection?")
-                    if fuel_injection == "yes":
-                        print("Check to ensure the choke is opening and closing.")
-                    elif fuel_injection == "no":
-                        print("Get it in for service.")
+silent_car = raw_input('Is the car silent when you turn the key?')
+if silent_car == "yes":
+    terminals_corroded = raw_input('Are the battery terminals corroded?')
+    if terminals_corroded == "yes":
+        print "Please clean terminals and try starting again."
+    elif terminals_corroded == "no":
+        print("The battery cables may be damaged. Replace cables and try again.")
     else:
         print ("Please enter either yes or no.")
-
-
-
-
+elif silent_car == "no":
+    clicking_noise = raw_input('Does the car make a clicking noise?')
+    if clicking_noise == "yes":
+        print("Replace the battery.")
+    elif clicking_noise == "no":
+        crank_up = raw_input('Does the car crank up but fail to start?')
+        if crank_up == "yes":
+            print("Check spark plug connections.")
+        elif crank_up == "no":
+            engine_die = raw_input('Does the engine start and then die?')
+            if engine_die == "yes":
+                fuel_injection = raw_input("Does your car have fuel injection?")
+                if fuel_injection == "yes":
+                    print("Get it in for service.")
+                elif fuel_injection == "no":
+                    print("Check to ensure the chock is opening and closing.")
+                else:
+                    print ("Please enter either yes or no.")
+            elif engine_die == "no":
+                print("Engine is not getting enough fuel. Clean fuel pump.")
+            else:
+                print ("Please enter either yes or no.")
+        else:
+            print ("Please enter either yes or no.")
+    else:
+        print ("Please enter either yes or no.")
+else:
+    print ("Please enter either yes or no.")
 diagnose_car()
