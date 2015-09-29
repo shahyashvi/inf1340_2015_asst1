39f06905056d122864f5925ff5c37cf052721efe
jeannemarie <jeannemarie.alfonso@mail.utoronto.ca>
2015-09-28 20:54:38 -0400

    Exercise 1

--- exercise1.py
+++ exercise1.py
@@ -13,15 +13,5 @@ __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 
-number_shares_bought = 2000
-#pricepershare
-price_shares_bought = 900
-stocks_price_bought_total = (price_shares_bought*number_shares_bought)
-commission_bought = stocks_price_bought_total*0.03
-
-number_shares_sold = 2000
-price_shares_sold = 942.75
-stocks_price_sold_total = price_shares_sold*number_shares_sold
-commission_sold = stocks_price_sold_total*0.03
-money = ((stocks_price_sold_total-commission_sold) - (stocks_price_bought_total+commission_bought))
-print ("Lakshmi has made a loss of because she has " + str(money) + " dollars left")
+money = 1000.00
+print(money)
