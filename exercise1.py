2ddcc3f26745bafce5a4a74c172296fa2b7e156f
jeannemarie <jeannemarie.alfonso@mail.utoronto.ca>
2015-09-29 15:53:04 -0400

    Exercise 1

--- exercise1.py
+++ exercise1.py
@@ -15,3 +15,21 @@ __license__ = "MIT License"
 
 money = 1000.00
 print(money)
+
+
+
+number_shares_bought = 2000
+shares_purchase_price = 900
+stock_purchase_price = (number_shares_bought*shares_purchase_price)
+commission_bought = (stock_purchase_price*0.03)
+total_stock_purchase_price = (stock_purchase_price+commission_bought)
+
+number_shares_sold = 2000
+shares_sold_price = 942.75
+stock_sold_price = (number_shares_sold*shares_sold_price)
+commission_sold = (stock_sold_price*0.03)
+total_stock_sold_price = (stock_sold_price-commission_sold)
+
+Profit = (total_stock_sold_price-total_stock_purchase_price)
+print("Lakshmi has " + str(Profit) + "dollars left.")
+
