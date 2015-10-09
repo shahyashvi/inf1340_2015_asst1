3ca1355b285b676c74b04a71a2c726226c6038ad
Yashvi <yashvi.c.shah@gmail.com>
2015-10-08 22:52:54 -0400

    Edited E1 + commented

--- exercise1.py
+++ exercise1.py
@@ -12,18 +12,21 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
+#calculations for purchasing stock
 number_shares_bought = 2000
 shares_purchase_price = 900
 stock_purchase_price = (number_shares_bought*shares_purchase_price)
 commission_bought = (stock_purchase_price*0.03)
 total_stock_purchase_price = (stock_purchase_price+commission_bought)
 
+#calculations for selling stock
 number_shares_sold = 2000
 shares_sold_price = 942.75
 stock_sold_price = (number_shares_sold*shares_sold_price)
 commission_sold = (stock_sold_price*0.03)
 total_stock_sold_price = (stock_sold_price-commission_sold)
 
+#calculation for total profit from processes
 Profit = (total_stock_sold_price-total_stock_purchase_price)
-print("Lakshmi has " + str(Profit) + "dollars left.")
+print("Lakshmi has " + str(Profit) + " dollars left.")
 
